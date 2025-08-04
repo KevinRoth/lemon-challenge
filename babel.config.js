40f705efa77b6befdd31a6bf70f970100bb36955
kevu <kevinrothlat@gmail.com>
2025-08-04 17:08:32 -0300

    setup dotenv and rtk query

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,15 @@ const plugin = require('tailwindcss');
 
 module.exports = {
   presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
-  plugins: ['react-native-worklets/plugin'],
+  plugins: [
+    'react-native-worklets/plugin',
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+      },
+    ],
+  ],
 };
